661825fc4563c30da93f710ce69a599b9e491d54
Giovanna Briano <gbrianor@gmail.com>
2025-06-20 12:39:16 -0700

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Giovanna Briano on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YouAreAwesomeTests/YouAreAwesomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeTests.swift
+//  YouAreAwesomeTests
+//
+//  Created by Giovanna Briano on 6/20/25.
+//
+
+import Testing
+@testable import YouAreAwesome
+
+struct YouAreAwesomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
